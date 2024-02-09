44508f1feb4d7af06c0e854d483847e9dde5fe4c
NKJ14 <147929768+NKJ14@users.noreply.github.com>
2024-02-09 20:33:40 +0530

    Update config.ts for jwt secret insertion

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,3 +1,3 @@
-const JWT_SECRET: string = "ShutUpMyMomsCalling";
+const JWT_SECRET: string = "ShutUpMyMomsCalling"; //add your own secret here instead
 
-export default JWT_SECRET;
+export default JWT_SECRET; 
